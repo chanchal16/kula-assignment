502a96f426f051a79847e6d85873842f8c19315b
chanchal16 <rajputchanchal17@gmail.com>
2022-11-13 23:28:08 +0530

    feat: usersSlice - create users slice

---
+++ src/features/Users/usersSlice.js
@@ -0,0 +1,40 @@
+import { createSlice, createAsyncThunk, } from '@reduxjs/toolkit'
+import axios from 'axios';
+
+const initialState = {
+    userList: [],
+    status:'idle',   
+}
+
+export const getUsers = createAsyncThunk("users/getUsers",async({searchQuery})=>{
+    console.log('query',searchQuery)
+    try{       
+       const {data}= await axios.get(`https://api.github.com/search/users?q=${searchQuery}`,{
+        headers: {
+            Accept: "application/vnd.github.v3+json"
+          }
+       } )
+       console.log('data',data.items)
+        return data.items       
+    }catch(err){console.error(err)}
+})
+
+export const usersSlice = createSlice({
+    name:'users',
+    initialState,
+    reducers:{},
+    extraReducers:(builder)=>{
+        builder
+        .addCase(getUsers.pending,(state)=>{
+            state.status = 'loading'
+        })
+        .addCase(getUsers.fulfilled,(state,action)=>{
+            state.status = 'fulfilled';
+            state.userList= action.payload
+        })
+        .addCase(getUsers.rejected,(state)=>{
+            state.status = 'rejected'
+        })
+    }
+})
+export default usersSlice.reducer
