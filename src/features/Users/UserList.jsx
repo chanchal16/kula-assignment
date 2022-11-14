6b3491d1182b0bacc8af7659197c8aa344d442a0
chanchal16 <rajputchanchal17@gmail.com>
2022-11-14 23:58:24 +0530

    update - add usercard component

--- src/features/Users/UserList.jsx
+++ src/features/Users/UserList.jsx
@@ -1,39 +1,28 @@
 import React from 'react';
-import { Box,Flex,WrapItem, Button,Text,Image, Link } from '@chakra-ui/react';
+import { Box } from '@chakra-ui/react';
 import { useSelector } from 'react-redux'
+import { UserCard } from '../../components/UserCard';
 
 export const UserList = () => {
     const {userList,status} = useSelector(state=>state.users)
   return (
     <section className='userlist-container'>
-        {/* {
-            status === 'fulfilled' &&  ( */}
-                <Box display='flex' flexDirection={'column'} alignItems='center' mx='auto' flexWrap={'wrap'} gap={6}>
+        {
+            status === 'loading' ? (
+                <Box display='flex' flexDirection={'column'} alignItems='center' mx='auto' gap={2}>
+                    <h3>loading...</h3>
+                </Box>
+            ) 
+            : (
+                <Box display='flex' flexDirection={'column'} alignItems='center' mx='auto' gap={6}>
                     {
                         userList?.map(user=>(
-                            <Box key={user.id}  w='2xl' p={2} m={2} border='1.5px' borderStyle='solid' borderColor='gray.200' borderRadius='5px'>
-                                <Flex align="center" justify="flex-start" gap={8}>
-                                    <WrapItem>
-                                        <Image boxSize='150px' alt={user.login} src={user.avatar_url} />{' '}
-                                    </WrapItem>
-                                    <Box>
-                                        <Text as='b' fontSize='xl'>{user.login}</Text>                                 
-                                        <Flex gap={8}>
-                                            <Text  fontSize='2xl'>{user.repos_url.length} <span  fontSize='xl'>Repos</span></Text>
-                                            <Text fontSize='2xl'>{user.followers_url.length} <span fontSize='xl'>Followers</span></Text>
-                                            <Text fontSize='2xl'>{user.following_url.length} <span fontSize='xl'>Following</span></Text>
-                                            <Text fontSize='2xl'>{user.gists_url.length} <span fontSize='xl'>Gists</span></Text>
-                                        </Flex>
-                                        <Link href={user.html_url} isExternal><Button size='sm' my='5px'>Github</Button></Link> 
-                                    </Box>
-                                </Flex>
-                                
-                            </Box>
+                            <UserCard user={user}/>                           
                         ))
                     }
                 </Box>
-            {/* )
-        }  */}
+            )
+        }  
     </section>
   )
 }
