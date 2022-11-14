e9458d63e10db7bcc27e2daded44548c00fbbadc
chanchal16 <rajputchanchal17@gmail.com>
2022-11-14 23:59:15 +0530

    feat:customRender - create custom render for tests

---
+++ src/utils/test-utils.js
@@ -0,0 +1,25 @@
+import { render as rtlRender } from '@testing-library/react';
+import { configureStore } from '@reduxjs/toolkit';
+import { Provider } from 'react-redux';
+import usersReducer from '../features/Users/usersSlice.js';
+
+function reducer(
+  ui,
+  {
+    preloadedState,
+    store = configureStore({ reducer: { users:usersReducer }, preloadedState }),
+    ...renderOptions
+  } = {}
+) {
+  function Wrapper({ children }) {
+    return (
+      <Provider store={store}>
+        {children}
+      </Provider>
+    );
+  }
+  return rtlRender(ui, { wrapper: Wrapper, ...renderOptions });
+}
+
+export * from '@testing-library/react';
+export { reducer };
