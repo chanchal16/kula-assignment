cd073767ae941856cbff7ffa8d0d7f6dfe7b4c3f
chanchal16 <rajputchanchal17@gmail.com>
2022-11-14 23:57:47 +0530

    feat: userCard - create usercard component

---
+++ src/components/UserCard.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Box,Flex,WrapItem, Button,Text,Image, Link } from '@chakra-ui/react';
+
+export const UserCard = ({user}) => {
+  return (
+    <Box data-testid='user' key={user.id}  w='2xl' p={2} m={2} border='1.5px' borderStyle='solid' borderColor='gray.200' borderRadius='5px'>
+        <Flex align="center" justify="flex-start" gap={8}>
+            <WrapItem>
+                <Image boxSize='150px' alt={user.login} src={user.avatar_url} />{' '}
+            </WrapItem>
+            <Box>
+                <Text as='b' fontSize='xl'>{user.login}</Text>                                 
+                <Flex gap={8}>
+                    <Text  fontSize='2xl'>{user.repos_url.length} <span  fontSize='xl'>Repos</span></Text>
+                    <Text fontSize='2xl'>{user.followers_url.length} <span fontSize='xl'>Followers</span></Text>
+                    <Text fontSize='2xl'>{user.following_url.length} <span fontSize='xl'>Following</span></Text>
+                    <Text fontSize='2xl'>{user.gists_url.length} <span fontSize='xl'>Gists</span></Text>
+                </Flex>
+                <Link href={user.html_url} isExternal><Button size='sm' my='5px'>Github</Button></Link> 
+            </Box>
+        </Flex>                              
+    </Box>
+  )
+}
