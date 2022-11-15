b7d1f5cc6f8391b89ba820663f7e1845d9506827
chanchal16 <rajputchanchal17@gmail.com>
2022-11-15 12:21:24 +0530

    chore - remove commented code

--- src/components/Form.test.js
+++ src/components/Form.test.js
@@ -1,30 +1,29 @@
-import {reducer, fireEvent, screen,cleanup, act,} from '../utils/test-utils.js';
+import {customRender, fireEvent, screen,cleanup, act,} from '../utils/test-utils.js';
 import { Form } from './Form';
 
 afterEach(cleanup)
 
-describe("(Component) Form",()=>{
-    it('inputing text updates the state',async()=>{
+describe("render Form component",()=>{
+    it('should render all form elements',async()=>{
         const mockOnSubmit = jest.fn()
-        const { getByPlaceholderText, getByText }= reducer(<Form  />)       
+        customRender(<Form  />)       
         
         const skillinput = screen.getByTestId('skill')
         const locationinput = screen.getByTestId('location')
         const submitBtn = screen.getByTestId('submit-btn')
 
-        // expect(getByText(/Change/i).textContent).toBe("Change: ")
         await act(async()=>{
-            fireEvent.change(getByPlaceholderText(/skills/i), {target: {value: 'js' } } )   
-            fireEvent.change(getByPlaceholderText(/location/i), {target: {value: 'delhi' } } )
+            fireEvent.change(skillinput, {target: {value: 'js' } } )   
+            fireEvent.change(locationinput, {target: {value: 'delhi' } } )
         })
         
         await act(async()=>{
             fireEvent.click(submitBtn)
         })
 
+        expect(skillinput.value).toBe('js');
+        expect(locationinput.value).toBe('delhi')
         expect(mockOnSubmit)
-    
-        // expect(getByText(/Change/i).textContent).not.toBe("Change: ")
     })
 })
 
