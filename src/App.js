42fe814d346480f8f52d9e04e4b07f43226cdacc
chanchal16 <rajputchanchal17@gmail.com>
2022-11-15 15:29:49 +0530

    style title

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import { Form } from './components/Form';
 import { UserList } from './features/Users/UserList';
+import {Text} from '@chakra-ui/react'
 
 function App() {
   return (
     <div className="App">
+      <Text fontSize='4xl' m={4} color='blue.400' fontWeight='bold' mx='auto'>Find Developers</Text>
       <Form/>
       <UserList/>
     </div>
